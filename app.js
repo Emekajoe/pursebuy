053cf8f67df342f3787d69aab9b9236819d38bee
Mac <mac@Macs-MacBook-Pro.local>
2022-12-03 17:56:07 +0100

    I updated the code

--- app.js
+++ app.js
@@ -10,6 +10,45 @@ const spending = document.querySelector(".spending");
 const accountImageBox = document.querySelector(".image-box");
 const transactionItems = document.querySelectorAll(".transaction__item");
 const mainContainer = document.querySelector(".main__container");
+const formBtn = document.querySelector(".form__btn");
+const formUser = document.querySelector(".form__username");
+const formPass = document.querySelector(".form__password");
+const accName = document.querySelector(".account__name");
+const formGroupLogin = document.querySelector(".form__group--login");
+
+const data = [
+  {
+    firstName: "Joseph",
+    last: "Aniebuo",
+    gender: "male",
+    country: "Nigeria",
+    username: "josephaniebuo",
+    password: "2222",
+  },
+  {
+    firstName: "Samuel",
+    last: "Hillary",
+    gender: "male",
+    country: "Hungary",
+    username: "samuelhillary",
+    password: "3333",
+  },
+];
+
+const logUser = function () {
+  const user = data.find((user) => user.username === formUser.value);
+
+  if (user.password === formPass.value) {
+    main.style.display = "flex";
+    accName.textContent = user.last;
+    formGroupLogin.style.visibility = "hidden";
+  }
+};
+
+formBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  logUser();
+});
 
 const nightMode = function () {
   if (toggleInput.checked) {
